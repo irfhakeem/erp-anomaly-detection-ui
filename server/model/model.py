7a3382ac3773c40526a1d482fa43eb8cf84c9e29
Naufal Khairul <naufalkr394@gmail.com>
2024-12-11 10:22:15 +0700

    fix: model logic

--- server/model/model.py
+++ server/model/model.py
@@ -11,6 +11,7 @@ import google.generativeai as genai
 import os
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 warnings.filterwarnings('ignore')
+from decouple import config
 
 # Inisialisasi Variabel
 amount = 0.0;
@@ -18,11 +19,23 @@ transaction_type = "";
 location = "";
 account_id = "";
 
-genai.configure(api_key=os.getenv("GENAI_API_KEY"))
+api_key = config('API_KEY')
+genai.configure(api_key=api_key)
 
 gemini = genai.GenerativeModel("gemini-1.5-flash")
 
-subset_df = pd.read_csv('./dataset/subset_df.csv')
+FILE_PATH = os.getcwd()
+# file = open(FILE_PATH+"\erp-anomaly-detection-ui\server\model\dataset\subset_df.csv", "rb")
+# subset_df = pd.read_csv(file)
+# subset_df_2 = pd.read_csv(file)
+# file.close()
+
+subset_df = pd.read_csv(FILE_PATH+'\server\model\dataset\subset_df.csv')
+subset_df_2 = pd.read_csv(FILE_PATH+'\server\model\dataset\subset_df.csv')
+
+# subset_df = pd.read_csv('/content/subset_df.csv')
+# subset_df_2 = pd.read_csv('/content/subset_df.csv')
+
 
 # Label encoder
 label_encoders = {}
@@ -115,6 +128,7 @@ def convert_to_dataframe(amount, transaction_type, location, account_id):
     df = pd.DataFrame(data)
     return df
 
+
 def load_models(file_path):
     with open(file_path, 'rb') as file:
         models = pickle.load(file)
@@ -131,78 +145,102 @@ def scale_amount(df):
     return df_copy
 
 # GEMINI
-def gemini_explanation(data, anomaly, user_input):
-    def check_outlier(df):
-      features = df[['Amount', 'TransactionType', 'Location', 'AccountID']]
-      max_feature = features.abs().idxmax()
+# GEMINI
+# Anomaly = df asli sebelum di encode dll
+# Shap output df = df hasil shap
+# data = subset_df atau yang lain (pokok yang masih ori aja tanpa encode)
+def gemini_explanation(data, anomaly_df, shap_output_df, user_input):
+    anomaly = anomaly_df.iloc[0]  # Ambil baris pertama jika ada banyak baris
 
-      return max_feature
+    total_contribution = shap_output_df['total_contribution'].iloc[0]
 
-    feature_name = check_outlier(anomaly)
+    if total_contribution > 0:
+        def check_outlier(df):
+            features = df[['Amount', 'TransactionType', 'Location', 'AccountID']]
+            max_feature = features.abs().sum().idxmax()
+            return max_feature
 
-    # Menghitung rata-rata untuk fitur Amount (contoh untuk kasus Amount)
-    if feature_name == "Amount":
-        non_anomaly_avg_amount = data[
-            (data['Location'] == anomaly['Location']) &
-            (data['AccountID'] == anomaly['AccountID']) &
-            (data['TransactionType'] == anomaly['TransactionType'])
-        ]['Amount'].mean()
+        feature_name = check_outlier(shap_output_df)
+        print(feature_name)
 
-        non_anomaly_avg_amount = non_anomaly_avg_amount if not pd.isna(non_anomaly_avg_amount) else 0
+        if feature_name == "Amount":
+            non_anomaly_avg_amount = data[
+                (data['Location'] == anomaly['Location']) &
+                (data['AccountID'] == anomaly['AccountID']) &
+                (data['TransactionType'] == anomaly['TransactionType'])
+            ]['Amount'].mean()
 
-        reason = (
-            f"The transaction amount of {anomaly['Amount']} is significantly "
-            f"different from the average transaction amount ({non_anomaly_avg_amount:.2f}) for "
-            f"similar accounts ({anomaly['AccountID']}), locations ({anomaly['Location']}), "
-            f"and transaction types ({anomaly['TransactionType']}). "
+            non_anomaly_avg_amount = non_anomaly_avg_amount if not pd.isna(non_anomaly_avg_amount) else 0
+
+            reason = (
+                f"The transaction amount of {anomaly['Amount']} is significantly "
+                f"different from the average transaction amount ({non_anomaly_avg_amount:.2f}) for "
+                f"similar accounts ({anomaly['AccountID']}), locations ({anomaly['Location']}), "
+                f"and transaction types ({anomaly['TransactionType']}). "
+            )
+        elif feature_name == "Location":
+            usual_location = data[
+                (data['AccountID'] == anomaly['AccountID']) &
+                (data['TransactionType'] == anomaly['TransactionType'])
+            ]['Location'].mode().iloc[0]
+
+            reason = (
+                f"The transaction originated from a location ({anomaly['Location']}) that is "
+                f"inconsistent with the usual locations ({usual_location}) for similar accounts ({anomaly['AccountID']}). "
+                f"This anomaly in location contributes to its classification as an outlier."
+            )
+        else:
+            reason = (
+                f"The transaction deviates significantly in terms of {feature_name}. "
+                f"This deviation suggests it does not align with expected behavior for similar transactions."
+            )
+
+        base_prompt = (
+            f"Explain why the transaction is considered an anomaly. "
+            f"{reason}"
+            f"Where, this transaction is one of several financial transaction records on the ERP Website System. "
+            f"Provide a detailed explanation to justify why this transaction is anomalous."
         )
 
-    # Kasus lain jika fitur lain menjadi penyebab (contoh: Location)
-    elif feature_name == "Location":
-        usual_location = data[
-            (data['AccountID'] == anomaly['AccountID']) &
-            (data['TransactionType'] == anomaly['TransactionType'])
-        ]['Location'].mode().iloc[0]
+        if user_input:
+            base_prompt += f" User asked: '{user_input}'"
 
-        reason = (
-            f"The transaction originated from a location ({anomaly['Location']}) that is "
-            f"inconsistent with the usual locations ({usual_location}) for similar accounts ({anomaly['AccountID']}). "
-            f"This anomaly in location contributes to its classification as an outlier."
-        )
+        print(base_prompt)
 
-    # Penjelasan umum
+        response = gemini.generate_content(base_prompt)
+        file_name = f"response_{anomaly['AccountID']}.txt"
+        with open(file_name, "w") as file:
+            file.write(response.text)
+
+        print(response.text)
+        return response.text
+
     else:
-        reason = (
-            f"The transaction deviates significantly in terms of {feature_name}. "
-            f"This deviation suggests it does not align with expected behavior for similar transactions."
+        # Jika total_contribution negatif, tidak ada anomaly
+        message = (
+            f"No ERP anomaly detected. All transactions appear normal based on the SHAP values {total_contribution:.3f}."
         )
+        file_name = f"response_{anomaly['AccountID']}.txt"
+        with open(file_name, "w") as file:
+            file.write(message)
 
-    # Prompt dasar yang disesuaikan
-    base_prompt = (
-        f"Explain why the transaction with ID {anomaly['TransactionID']} is considered an anomaly. "
-        f"{reason}"
-        f"Where, this transaction is one of several financial transaction records on the ERP Website System. "
-        f"The anomaly was detected by our model for {anomaly['Merchant']}. "
-        f"Provide a detailed explanation to justify why this transaction is anomalous."
-    )
+        print(message)
+        return message
 
-    if user_input:
-        base_prompt += f" User asked: '{user_input}'"
+file_path = FILE_PATH+r'\server\model\trained_models.pkl'
 
-    print(base_prompt)  # Output prompt untuk pengecekan
+# Load model dari file
+models = load_models(file_path)
 
-    # Menghasilkan penjelasan menggunakan model Gemini
-    response = gemini.generate_content(base_prompt)
+# Load model dari file
+model1, model2, model3, model4, model5 = models
 
-    # Simpan hasil dalam file
-    file_name = f"response_{anomaly['TransactionID']}.txt"
-    with open(file_name, "w") as file:
-        file.write(response.text)
+# Contoh tidak anomali
+# prompt = "I have a transaction with Amount 68629.69, Transaction Type Transfer, Location London, Account ID ACC5." 
+# Contoh anomaly
+prompt = "I have a transaction with Amount 87.87, Transaction Type Purchase, Location London, Account ID ACC6."
 
-    # Output hasil text
-    print(response.text)
-    return response.text
-
+process_user_prompt(prompt)
 
 df_row = convert_to_dataframe(amount, transaction_type, location, account_id)
 print(df_row)
@@ -210,27 +248,52 @@ print(df_row)
 encoded_df = apply_and_update_encoding(df_row)
 print(encoded_df)
 
-final_df = scale_amount(encoded_df)
+encoded_subset_df = apply_and_update_encoding(subset_df_2)
+print(encoded_subset_df.head(10))
+
+final_df = encoded_df.copy()
+
+mms = MinMaxScaler(feature_range=(0, 1))
+mms.fit(encoded_subset_df[['Amount']])  # Fit hanya sekali pada subset_df
+
+final_df['Amount'] = mms.transform(final_df[['Amount']])
+
 print(final_df)
+# final_df = scale_amount(encoded_df)
+# final_df = final_df.head(1)
+# print(final_df)
 
-models = load_models('./trained_models.pkl')
-model1, model2, model3, model4, model5 = models
 
-# SHAP
+import shap
+
+# Calculate SHAP values for each model
 shap_values_list = []
 
 for fold, model in enumerate(models):
     explainer = shap.Explainer(model)
-    shap_values = explainer(final_df)
+    shap_values = explainer(final_df)  # SHAP values for test set in this fold
     shap_values_list.append(shap_values)
 
-shap_values_mean = np.mean([shap_values.values for shap_values in shap_values_list], axis=0)
+# Aggregate SHAP values (mean across folds)
+shap_values_mean_3 = np.mean([shap_values.values for shap_values in shap_values_list], axis=0)
+
+# # Dapatkan nilai SHAP per fitur
+# shap_values_df = pd.DataFrame(shap_values.values, columns=X_test.columns)
+# shap_values_df['total_contribution'] = shap_values_df.sum(axis=1)
+
+# # VISUALISASI NON OUTLIER (Index 0)
+# shap.initjs()
+# shap.plots.force(shap_values_mean[0])
 
 # Buat DataFrame dari rata-rata SHAP values
-shap_values_mean_df = pd.DataFrame(
-    shap_values_mean,
+shap_values_mean_df_3 = pd.DataFrame(
+    shap_values_mean_3,
     columns=final_df.columns  # Nama fitur
 )
-shap_values_mean_df['total_contribution'] = shap_values_mean_df.sum(axis=1)  # Total kontribusi per baris
+shap_values_mean_df_3['total_contribution'] = shap_values_mean_3.sum(axis=1)  # Total kontribusi per baris
 
-gemini_explanation(subset_df, shap_values_mean_df, "");
+# Tampilkan DataFrame
+shap_values_mean_df_3
+print(shap_values_mean_df_3)
+
+gemini_explanation(subset_df_2, df_row, shap_values_mean_df_3, "")
